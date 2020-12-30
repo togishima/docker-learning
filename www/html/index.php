8024aa153c0c4b1d52b8fc1c3f4d01847187b2e0
togishima <ogishima-t@DESKTOP-RVV61NJ.localdomain>
2020-12-30 11:32:43 +0900

    initial

---
+++ www/html/index.php
@@ -0,0 +1,17 @@
+<?php
+    echo $_SERVER['REMOTE_ADDR'];
+?>
+<!-- <!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Dockerのテスト</h1>
+    <?php phpinfo() ?>
+</body>
+</html> -->
+
+!
